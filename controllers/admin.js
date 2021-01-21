2c574040d66cbbbdcc40869661760aa3d5f24161
Govert <car16116@byui.edu>
2021-01-21 16:35:11 -0700

    Prove for week0 and ecommerce setup

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,8 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
-    path: '/admin/add-product'
+    path: '/admin/add-product',
+    editing: false
   });
 };
 
@@ -12,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null,title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -21,11 +22,31 @@ exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) 
     res.redirect('/');
-  res.render('admin/edit-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    editing: editMode
+
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) 
+      res.redirect('/');
+
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/add-product',
+      editing: editMode,
+      product: product
+    });
   });
+  
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.prodId;
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(prodId,title, imageUrl, description, price);
+  product.save();
+  res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
--- models/product.js
+++ models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,12 +27,24 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
+    
+    
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if (this.id) {
+        const exisProdIndex = products.findIndex(p => p.id === this.id);
+        const updateProd = [... products];
+        updateProd[exisProdIndex] = this;
+        fs.writeFile(p, JSON.stringify(updateProd), err => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(productsx), err => {
+          console.log(err);
+        });
+      }
+      
     });
   }
 
